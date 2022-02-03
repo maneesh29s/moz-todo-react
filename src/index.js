6e5d1090c888520e4e446dd79f17b06b0072b0b8
maneesh29s <sutarmaneesh00@gmail.com>
2022-02-03 22:23:13 +0530

    completed 3rd page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import App from './App';
 
 // whole tutorial is on: https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_getting_started
 
-ReactDOM.render(<App subject="Clarice" />, document.getElementById('root'));
+const DATA = [
+    { id: "todo-0", name: "Eat", completed: true },
+    { id: "todo-1", name: "Sleep", completed: false },
+    { id: "todo-2", name: "Repeat", completed: false }
+  ];
+
+ReactDOM.render(<App tasks={DATA} />, document.getElementById('root'));
 
 
